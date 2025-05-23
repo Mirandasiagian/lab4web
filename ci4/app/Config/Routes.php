75ba9484d3bec3900da3037b8c149130c3c7abb9
Mirandasiagian <mirandasiagian198@gmail.com>
2025-05-23 07:07:10 +0700

    Menambahakan App dan memperbaiki Routes

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -10,3 +10,5 @@ $routes->get('/about', 'Page::about');
 $routes->get('/contact', 'Page::contact');
 $routes->get('/faqs', 'Page::faqs');
 $routes->get('/tos', 'Page::tos' );
+$routes->get('/user/logi', 'User::login');
+$routes->setAutoRoute(true);
